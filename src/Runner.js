8550b3333e2736c88ce0cdc2406346604214b80c
Omar Chehab <omarchehab98@gmail.com>
2017-11-10 19:37:58 -0500

    Update Runner.js
    Bind `this` context to `runTask`.\nReturn promise in `runTask`.\nWrite external function documentation.\nRestart runner if `start` is called when already running.

--- src/Runner.js
+++ src/Runner.js
@@ -6,26 +6,43 @@ function Runner(task, interval) {
   this.task = task
   this.interval = interval
   this.taskIsFinished = true
+  this.runTask = this.runTask.bind(this)
 }
 
+/**
+ * Starts the runner.
+ * 
+ * Restarts the runner if it is already runner.
+ */
 Runner.prototype.start = function start() {
+  this.stop();
   this.intervalId = setInterval(this.runTask, this.interval)
 }
 
+/**
+ * Runs the task with the last task's result as the parameter.
+ * 
+ * If the previous task is not finished, then the task is not run.
+ */
 Runner.prototype.runTask = function runTask() {
   if (!this.taskIsFinished) {
     return
   }
   this.taskIsFinished = false
-  this.task(this.lastResult)
-    .then((result) => {
-      this.lastResult = result
-      this.taskIsFinished = true
-      resolve(result)
-    })
-    // .catch(reject)
+  return new Promise((resolve, reject) => {
+    this.task(this.lastResult)
+      .then((result) => {
+        this.lastResult = result
+        this.taskIsFinished = true
+        resolve(result)
+      })
+      .catch(reject)
+  })
 }
 
+/**
+ * Stops the runner.
+ */
 Runner.prototype.stop = function stop() {
   clearInterval(this.intervalId)
 }
